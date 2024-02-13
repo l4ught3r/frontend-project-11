f4b0ceb581205dbde44d46d77b1c7054b250412d
Anton Kuchin <l4ught3r@mail.ru>
2024-02-13 16:18:31 +0700

    make fix

--- src/tracking.js
+++ src/tracking.js
@@ -5,21 +5,19 @@ import proxy from './proxy';
 
 const tracking = (state) => {
   const { feeds, posts } = state;
-  const promises = feeds.map(({ url, id }) =>
-    axios
-      .get(proxy(url))
-      .then((response) => parser(response.data))
-      .then((channel) => {
-        const updatedPosts = channel.posts;
-        const oldPosts = posts.filter((post) => post.feedId === id);
-        const addedPosts = _.differenceBy(updatedPosts, oldPosts, 'link');
-        if (addedPosts.length !== 0) {
-          const newPosts = addedPosts.map((post) => ({ ...post, id: _.uniqueId(), feedId: id }));
-          state.posts = [...newPosts, ...posts];
-        }
-      })
-      .catch((err) => console.log(err)),
-  );
+  const promises = feeds.map(({ url, id }) => axios
+    .get(proxy(url))
+    .then((response) => parser(response.data))
+    .then((channel) => {
+      const updatedPosts = channel.posts;
+      const oldPosts = posts.filter((post) => post.feedId === id);
+      const addedPosts = _.differenceBy(updatedPosts, oldPosts, 'link');
+      if (addedPosts.length !== 0) {
+        const newPosts = addedPosts.map((post) => ({ ...post, id: _.uniqueId(), feedId: id }));
+        state.posts = [...newPosts, ...posts];
+      }
+    })
+    .catch((err) => console.log(err)));
   Promise.all(promises).then(() => setTimeout(() => tracking(state), 5000));
 };
 
