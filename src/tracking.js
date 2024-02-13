44c81815c68499790c6bd67c29de8efa9b210f1d
Anton Kuchin <l4ught3r@mail.ru>
2024-02-13 16:14:31 +0700

    tracking.js Promise.all

--- src/tracking.js
+++ src/tracking.js
@@ -5,20 +5,22 @@ import proxy from './proxy';
 
 const tracking = (state) => {
   const { feeds, posts } = state;
-  feeds.forEach(({ url, id }) => axios
-    .get(proxy(url))
-    .then((response) => parser(response.data))
-    .then((channel) => {
-      const updatedPosts = channel.posts;
-      const oldPosts = posts.filter((post) => post.feedId === id);
-      const addedPosts = _.differenceBy(updatedPosts, oldPosts, 'link');
-      if (addedPosts.length !== 0) {
-        const newPosts = addedPosts.map((post) => ({ ...post, id: _.uniqueId(), feedId: id }));
-        state.posts = [...newPosts, ...posts];
-      }
-    })
-    .catch((err) => console.log(err)));
-  setTimeout(() => tracking(state), 5000);
+  const promises = feeds.map(({ url, id }) =>
+    axios
+      .get(proxy(url))
+      .then((response) => parser(response.data))
+      .then((channel) => {
+        const updatedPosts = channel.posts;
+        const oldPosts = posts.filter((post) => post.feedId === id);
+        const addedPosts = _.differenceBy(updatedPosts, oldPosts, 'link');
+        if (addedPosts.length !== 0) {
+          const newPosts = addedPosts.map((post) => ({ ...post, id: _.uniqueId(), feedId: id }));
+          state.posts = [...newPosts, ...posts];
+        }
+      })
+      .catch((err) => console.log(err)),
+  );
+  Promise.all(promises).then(() => setTimeout(() => tracking(state), 5000));
 };
 
 export default tracking;
